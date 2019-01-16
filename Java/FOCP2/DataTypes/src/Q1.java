c101717e17302af1d7f9098bba5bf89daa6add46
dhruvindoria <46585530+dhruvindoria@users.noreply.github.com>
2019-01-16 09:15:35 +0530

    Add files via upload

---
+++ Java/FOCP2/DataTypes/src/Q1.java
@@ -0,0 +1,12 @@
+
+public int getArrayIndex(int[] arr,int value) {
+
+        int k=0;
+        for(int i=0;i<arr.length;i++){
+
+            if(arr[i]==value){
+                k=i;
+                break;
+            }
+        }
+    return k; 
---
+++ Java/FOCP2/DataTypes/src/operators.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Operators {
+	public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+int a=3, b=4, add, sub, div, multi;
+add=a+b;
+sub=a-b;
+div=a/b;
+mod=a%b;
+multi=a*b;
+
+System.out.println("add");
+System.out.println("sub");
+System.out.println("div");
+System.out.println("mod");
+System.out.println("multi");
+
+	}
+}
